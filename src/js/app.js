98bc9f71c8729b375448d4244a294358df20949e
Svetlana Kutyova <kutyovas@yandex.ru>
2022-01-17 20:19:17 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import renderBoard from './board';
 
 const newBoard = [];
 const fieldSize = 4;
+const imgEl = document.createElement('img');
+imgEl.src = 'E:/0-Web-учеба/0-блок8-js-move/ahj2-dom/src/img/goblin.png';
+imgEl.alt = 'Текущее положение';
+console.log(imgEl);
+// imgEl.textContent = 'Click me';
 // const boardEl = document.getElementById('board');
 
 function fillNewBoard() {
@@ -9,10 +14,10 @@ function fillNewBoard() {
   for (let i = 0; i < fieldSize; i += 1) {
     newBoard[i] = [];
     for (let j = 0; j < fieldSize; j += 1) {
-      console.log(i);
-      console.log(j);
+      // console.log(i);
+      // console.log(j);
       newBoard[i][j] = '';
-      console.log(newBoard);
+      // console.log(newBoard);
     }
   }
 }
